bad6b60d35c24e449225b193c42c91c84ef4e41f
superphil0 <philippomenitsch@gmx.at>
2014-03-23 20:18:00 +0100

    work in progress

--- server/clientSide.js
+++ server/clientSide.js
@@ -7,92 +7,110 @@ var winston = require('winston');
 GET Methods:
 
 -login { key : generated key
-		 deviceid : }
-		 return: token which can be used to open the door
-		 
+deviceid : }
+return: token which can be used to open the door
+
 -opendoor { generated token in server handshake }
-		return error or success message
-		
+return error or success message
+
 -generate: this generates a new passphrase which can be handed out to users
-		{ master password }
-		return passphrase
+{ master password }
+return passphrase
 
 
-*/
+ */
 
 // begin
-var app = express(); 
+var app = express();
 app.use(express.json());
-var addCORSHeaders = function (req, res) {
-    res.setHeader("Access-Control-Max-Age", "300")
-    res.setHeader("Access-Control-Allow-Origin", req.headers['origin'])
-    res.setHeader("Access-Control-Allow-Credentials", "true")
+var addCORSHeaders = function (req, res)
+{
+	res.setHeader("Access-Control-Max-Age", "300")
+	res.setHeader("Access-Control-Allow-Origin", req.headers['origin'])
+	res.setHeader("Access-Control-Allow-Credentials", "true")
 
-    if (req.headers.hasOwnProperty("access-control-request-method")) {
-        res.setHeader("Access-Control-Allow-Methods", req.header['access-control-request-method']);
-    }
+	if (req.headers.hasOwnProperty("access-control-request-method"))
+	{
+		res.setHeader("Access-Control-Allow-Methods", req.header['access-control-request-method']);
+	}
 
-    if (req.headers.hasOwnProperty("access-control-request-headers")) {
-        res.setHeader("Access-Control-Allow-Headers", req.header['access-control-request-headers']);
-    }
+	if (req.headers.hasOwnProperty("access-control-request-headers"))
+	{
+		res.setHeader("Access-Control-Allow-Headers", req.header['access-control-request-headers']);
+	}
 };
 
 // add CORS headers to every request
-app.all(config.path + '/*', function (req, res, next) {
-    addCORSHeaders(req, res);
-    next();
-});
+app.all(config.path + '/*', function (req, res, next)
+{
+	addCORSHeaders(req, res);
+	next();
+}
+);
 
-app.get(config.path + '/login', function (req, res) {
-	query = url.parse(req.url,true).query;
+app.get(config.path + '/login', function (req, res)
+{
+	query = url.parse(req.url, true).query;
 	var key = query.key;
-    var deviceid = parseInt(query.deviceid);
-	serverSide.login(deviceid, key, function(deviceInfo) {
+	var deviceid = parseInt(query.deviceid);
+	serverSide.login(deviceid, key, function (deviceInfo)
+	{
 		winston.info(deviceInfo);
-		if(deviceInfo)
+		if (deviceInfo)
 		{
 			winston.info("login from user");
 			res.send(deviceInfo);
 		}
 		else
-		{ 
+		{
 			res.status(401).send();
 		}
-	});
-});
+	}
+	);
+}
+);
 
-app.get(config.path + '/opendoor', function (req, res) {
-	query = url.parse(req.url,true).query;
+app.get(config.path + '/opendoor', function (req, res)
+{
+	query = url.parse(req.url, true).query;
 	var token = query.token;
-    var deviceid = parseInt(query.deviceid);
+	var deviceid = parseInt(query.deviceid);
 	var doorNumber = parseInt(query.doorNumber);
 
-	serverSide.opendoor(deviceid, doorNumber, token, function(success) {
-		if(success)
+	serverSide.opendoor(deviceid, doorNumber, token, function (success)
+	{
+		if (success)
 		{
-			winston.info("user opened door "+ deviceid + " door: " + doorNumber);
-			res.send({success: success});
+			winston.info("user opened door " + deviceid + " door: " + doorNumber);
+			res.send(
+			{
+				success : success
+			}
+			);
 		}
 		else
-		{ 
-			res.status(401).send({success: success});
+		{
+			res.status(401).send(
+			{
+				success : success
+			}
+			);
 		}
-	});
-});
+	}
+	);
+}
+);
 
+app.get(config.path + '/generate', function (req, res)  {}
 
-app.get(config.path + '/generate', function (req, res) {
-   
-   
-});
+);
 
 exports.app = app;
-exports.start = function()
+exports.start = function ()
 {
-for(var i=1;i<15;i++)
-{
-	serverSide.createDevice(i,i%3?[1,2]:[1],i%3);
-}
+	for (var i = 1; i < 15; i++)
+	{
+		serverSide.createDevice(i, i % 3 ? [1, 2] : [1], i % 3);
+	}
 
 }
-
--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 exports.portRasp = 3000;
 exports.portClient = 3001;
 exports.path = '/opendoor';
-exports.mongoURL = '127.0.0.1:27017/opendoor'
+exports.mongoURL = '127.0.0.1:27017/opendoor';
+exports.defaultBuzzTime = 5000;
