19c6717e336c93aa05461668bd87ec330f0d5779
superphil0 <philippomenitsch@gmx.at>
2014-03-23 15:04:17 +0100

    fixed opendoor function

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 exports.portRasp = 3000;
 exports.portClient = 3001;
 exports.path = '/opendoor';
-exports.mongoURL = 'dev:devpw@192.168.1.3:27017/opendoor'
+exports.mongoURL = '127.0.0.1:27017/opendoor'
