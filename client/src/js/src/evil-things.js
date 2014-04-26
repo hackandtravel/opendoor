ca7b6a6ffa17f4dd72088584c4cd5496a3f4378d
Florian Klampfer <f.klampfer@gmail.com>
2014-04-26 22:05:02 +0200

    add flatten to array prototype

--- client/src/js/src/evil-things.js
+++ client/src/js/src/evil-things.js
@@ -11,4 +11,12 @@ define([
       return this.slice(-str.length) == str;
     };
   }
+
+  if (typeof Array.prototype.flatten != 'function') {
+    Array.prototype.flatten = function () {
+      return this.reduce(function (sum, next) {
+        return sum.concat(next);
+      }, []);
+    };
+  }
 });
