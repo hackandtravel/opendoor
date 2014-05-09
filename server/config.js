938c6326d69a1b70989dec7857472cfdf2985e48
docker Philipp <philipp.omenitsch@gmail.com>
2014-05-09 18:07:35 -0400

    also load ca file

--- server/config.js
+++ server/config.js
@@ -4,8 +4,9 @@ var port = process.env.DB_PORT_27017_TCP_PORT;
 exports.portRasp = 3000;
 exports.portClient = 3001;
 exports.path = '/opendoor';
-exports.mongoURL = 'mongodb://localhost:27017/test';
-exports.defaultBuzzTime = 5000;
-exports.keyLength = 4;
-exports.debug = 1;
+exports.debug = 0;
 
+exports.mongoURL = 'mongodb://' + addr + ':' + port + '/test';
+exports.defaultBuzzTime = 5000;
+exports.keyLength = 6;
+exports.deviceidLength=4;
