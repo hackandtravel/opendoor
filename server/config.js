be4ec8c42e7e4abe5988eca19c2876fd837f50b4
Philipp Omenitsch <philippomenitsch@gmx.at>
2014-05-10 04:32:39 +0200

    mongo changed

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ exports.portClient = 3001;
 exports.path = '/opendoor';
 exports.debug = 0;
 
-exports.mongoURL = 'mongodb://' + addr + ':' + port + '/test';
+exports.mongoURL = 'mongodb://localhost:27017/test';
 exports.defaultBuzzTime = 5000;
 exports.keyLength = 6;
 exports.deviceidLength=4;
