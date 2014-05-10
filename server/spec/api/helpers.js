b82ed04faa6ad79ca9898fdbcc81d0c7f0e798f2
Philipp Omenitsch <philippomenitsch@gmx.at>
2014-05-10 04:40:22 +0200

    mongo changed

--- server/spec/api/helpers.js
+++ server/spec/api/helpers.js
@@ -6,8 +6,8 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var Promise = require('es6-promise').Promise;
 var request = require('request');
-exports.url = 'http://localhost:3001/opendoor/';
-var urli = 'http://localhost:3001/opendoor/';
+exports.url = 'https://localhost:3001/opendoor/';
+var urli = 'https://localhost:3001/opendoor/';
 exports.admin = "admin";
 var admin = "admin";
 exports.pwd = "password";
