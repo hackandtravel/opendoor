a201c94066515f25ebd715d414a25822ebba2a71
= <=>
2014-05-15 20:57:49 +0200

    debug mode off

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 exports.portRasp = 3000;
 exports.portClient = 3001;
 exports.path = '/opendoor';
-exports.debug = 1;
+exports.debug = 0;
 
 exports.mongoURL = 'mongodb://localhost:27017/test';
 exports.defaultBuzzTime = 5000;
